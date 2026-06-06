10a7e05c48b816ca4830879000d1eb01a0f0881b
rex747 <36256042+rex747@users.noreply.github.com>
2026-06-06 10:27:20 +0300

    Changes 06.06.26

--- app/src/main/java/com/example/myapplication/ui/screens/MyAgentsScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/screens/MyAgentsScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.*
